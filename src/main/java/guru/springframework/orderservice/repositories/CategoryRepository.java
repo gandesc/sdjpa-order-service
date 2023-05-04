84983a490688daf37a75eb70338d80ecd8c614c4
gandesc <mihai.ocneanu@gmail.com>
2023-05-04 08:39:37 +0300

    added category repository

---
+++ src/main/java/guru/springframework/orderservice/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.orderservice.repositories;
+
+import guru.springframework.orderservice.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByDescription(String description);
+}
