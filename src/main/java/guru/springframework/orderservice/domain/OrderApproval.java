79271b293062a0914bf57df4072cd61c44b40297
gandesc <mihai.ocneanu@gmail.com>
2023-05-10 14:04:44 +0300

    added an OrderApproval 1-1 relationship

---
+++ src/main/java/guru/springframework/orderservice/domain/OrderApproval.java
@@ -0,0 +1,2 @@
+package guru.springframework.orderservice.domain;public class OrderApproval {
+}
---
+++ src/main/java/guru/springframework/orderservice/repositories/OrderApprovalRepository.java
@@ -0,0 +1,2 @@
+package guru.springframework.orderservice.repositories;public interface OrderApprovalRepository {
+}
